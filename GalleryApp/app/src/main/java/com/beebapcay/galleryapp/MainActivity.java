62232d62363657c39090e2c09e2676b74fde250f
Phát Dương <69242061+beebapcay@users.noreply.github.com>
2021-03-08 18:16:10 +0700

    Complete: Bottom Navigation Bar

--- GalleryApp/app/src/main/java/com/beebapcay/galleryapp/MainActivity.java
+++ GalleryApp/app/src/main/java/com/beebapcay/galleryapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.beebapcay.galleryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        NavController navController = (NavController) Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
