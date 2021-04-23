a378000f46fe1cd97c774d91a4a667351e369d54
Phát Dương <69242061+beebapcay@users.noreply.github.com>
2021-04-23 13:42:24 +0700

    Create: StickyActionBar & QuickOptionButton (Layout)

---
+++ GalleryApp/app/src/main/java/com/beebapcay/galleryapp/views/fragments/QuickOptionButtonFragment.java
@@ -0,0 +1,28 @@
+package com.beebapcay.galleryapp.views.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.beebapcay.galleryapp.R;
+
+
+public class QuickOptionButtonFragment extends Fragment {
+
+    public QuickOptionButtonFragment() { }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_quick_option_button, container, false);
+    }
+}
